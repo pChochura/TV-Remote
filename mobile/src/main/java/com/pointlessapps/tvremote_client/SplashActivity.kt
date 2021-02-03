0d1288f721800acd1caca218019f1e66f42e098d
pChochura <pchochura@gmail.com>
2021-02-03 21:40:43 +0100

    Added splash screen

---
+++ mobile/src/main/java/com/pointlessapps/tvremote_client/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.pointlessapps.tvremote_client
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
